9bc5ce7c67f330f0f697676c4efbd03fa76a1791
Nikitazurange30 <nikita.zurange30@gmail.com>
2026-05-13 16:54:07 +0530

    view

---
+++ src/main/java/com/todo/servelet/ViewServlet.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.util.List;
+
+import com.todo.dao.TaskDAO;
+import com.todo.model.Task;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/ViewServlet")
+
+public class ViewServlet extends HttpServlet
+{
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		TaskDAO dao = new TaskDAO();
+		List<Task> taskList = dao.getAllTasks();
+		
+		request.setAttribute("taskList", taskList);
+		request.getRequestDispatcher("dashboard.html").forward(request,response);
+		
+	}
+}
